730508ab148146fea7900df829504350678191e2
Vandann-1 <vandanparmar6041@gmail.com>
2025-03-20 17:25:32 +0530

    first commit

---
+++ invapp/views.py
@@ -0,0 +1,27 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        user_data = UserSerializer(user).data
+        return Response({
+            "message": "Login successful",
+            "token": str(refresh.access_token),
+            "refresh": str(refresh),
+            "user": user_data
+        })
+    else:
+        return Response({"message": "Invalid credentials"}, status=400)
+
+@api_view(['GET'])
+def protected_view(request):
+    return Response({"message": f"Hello, {request.user.username}"})
