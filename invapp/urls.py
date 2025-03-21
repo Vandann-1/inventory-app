4ecb0f8f3535bb09b9c2a1c4be0d206f81a7383f
unknown <kalathiyajevin77@gmail.com>
2025-03-21 12:36:04 +0530

    PY

--- invapp/urls.py
+++ invapp/urls.py
@@ -1,9 +1,10 @@
 
 
 from django.urls import path
-from .views import login, protected_view
+from invapp.views import *
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('register/', registerv, name='register'),
     path('protected/', protected_view, name='protected'),
 ]
--- invapp/views.py
+++ invapp/views.py
@@ -2,7 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import UserSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 @api_view(['POST'])
 def login(request):
@@ -25,3 +29,20 @@ def login(request):
 @api_view(['GET'])
 def protected_view(request):
     return Response({"message": f"Hello, {request.user.username}"})
+
+def registerv(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+            else:
+                User.objects.create_user(username=username, password=password)
+                messages.success(request, 'Registration successful')
+                return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+    return render(request, 'register.php')
